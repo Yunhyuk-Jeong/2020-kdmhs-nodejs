905ae45c9f25692af9140bc5b619a27312b19b09
정윤혁 <superjyh02@naver.com>
2020-05-28 22:17:33 +0900

    200528 study

---
+++ 20_nodejs/21_basic/3-2_fs.js
@@ -0,0 +1,39 @@
+// https://nodejs.org/dist/latest-v12.x/docs/api/fs.html
+var fs = require("fs");
+
+// 스트림 생성
+var debug = fs.createWriteStream("debug.log");
+var error = fs.createWriteStream("error.log");
+
+// 콘솔 클래스 얻기
+var Console = console.Console;
+
+// 콘솔 객체 생성
+var logger = new Console(debug, error);
+
+logger.log("log message");
+logger.info("info message");
+logger.warn("warn message");
+logger.error("error message");
+
+// 동기식
+try {
+  var data = fs.readFileSync("3-3_hello.txt", "utf-8");
+  console.log(data);
+} catch (exception) {
+  console.error("동기식 Error : " + exception);
+  return;
+}
+
+console.log("동기식 읽기 완료");
+
+// 비동기식
+fs.readFile("3-3_hello.txt", "utf-8", function (err, data) {
+  if (err) {
+    console.error("비동기식 Error : " + err);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("비동기식 읽기 완효");
---
+++ 20_nodejs/21_basic/3-4_url.js
@@ -0,0 +1,12 @@
+// https://nodejs.org/dist/latest-v12.x/docs/api/url.html
+var url = require("url");
+
+var urlPath =
+  "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=node.js";
+
+var parsed = url.parse(urlPath, true); // parse : String -> Object 변환
+console.log(parsed);
+
+console.log(parsed.host); // search.naver.com
+console.log(parsed.path); // /search.naver?sm=top_hty&fbm=1&ie=utf8&query=node.js
+console.log(parsed.query); // ? 뒤의 query값을 객체로 리턴
